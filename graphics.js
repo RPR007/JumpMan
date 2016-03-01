08896a8cb45692ef99c7fe33d94f012545dc6906
Rafael Prevost-Rivero <rafael@Light.home>
2016-03-01 14:45:38 -0500

    ajout de jumpman

--- graphics.js
+++ graphics.js
@@ -34,10 +34,103 @@
      context.restore()
    }
 
+
    function drawRope(pX , pY) {
      context.save()
      
      var rope = [[true,false],[false,true],[true,false],[false,true],[true,false],[false,true],[true,false],[false,true]]
+     pX *= dimPx;pY *= dimPx
+     
+     context.fillStyle = 'green'
+     
+     for(var i = 0; i < rope.length; i++)
+        for(var j = 0; j < rope[i].length;j++)
+            if(rope[i][j])
+                context.fillRect(pX+(j*dimPx) ,pY+(i*dimPx) , dimPx , dimPx)
+                
+     context.restore()
+   }
+   
+   function drawJumpMan(pX , pY) {
+     context.save()
+     
+     var head = [[false,false,false,false,true,true,true,true,false,false,false,false]
+                ,[false,false,false,false,true,true,true,true,false,false,false,false]]
+     var body = [[false,false,true,true,true,true,true,true,true,true,false,false]
+                ,[true,true,false,false,true,true,true,true,false,false,true,true]
+                ,[false,false,true,true,true,true,true,true,true,true,false,false]]
+     var leg = [[false,false,false,false,true,true,true,true,false,false,false,false]
+               ,[false,false,false,false,true,true,true,true,false,false,false,false]
+               ,[false,false,false,false,true,true,true,true,false,false,false,false]
+               ,[false,false,false,false,true,true,true,true,false,false,false,false]]
+     var feet = [[false,false,true,true,true,true,true,true,true,true,false,false]]
+     
+     pX *= dimPx;pY *= dimPx
+     
+     // tete
+     context.fillStyle = 'white'
+     for(var i = 0; i < head.length; i++)
+        for(var j = 0; j < head[i].length;j++)
+            if(head[i][j])
+                context.fillRect(pX+(j*dimPx) ,pY+(i*dimPx) , dimPx , dimPx)
+        
+     // corps
+     pY += (2*dimPx) 
+     context.fillStyle = 'brown'
+     for(var i = 0; i < body.length; i++)
+        for(var j = 0; j < body[i].length;j++)
+            if(body[i][j])
+                context.fillRect(pX+(j*dimPx) ,pY+(i*dimPx) , dimPx , dimPx)
+               
+     // jambe
+     pY += (3*dimPx) 
+     context.fillStyle = 'purple'
+     for(var i = 0; i < leg.length; i++)
+        for(var j = 0; j < leg[i].length;j++)
+            if(leg[i][j])
+                context.fillRect(pX+(j*dimPx) ,pY+(i*dimPx) , dimPx , dimPx)
+                
+     // pied
+     pY += (4*dimPx)
+     context.fillStyle = 'white'
+     for(var i = 0; i < feet.length; i++)
+        for(var j = 0; j < feet[i].length;j++)
+            if(feet[i][j])
+                context.fillRect(pX+(j*dimPx) ,pY+(i*dimPx) , dimPx , dimPx) 
+                
+     context.restore()
+   }
+   
+   function drawJumpManLeft(pX , pY) {
+     context.save()
+     
+     var head = []
+     var body = []
+     var hand = []
+     var leg = []
+     var feet = []
+     
+     pX *= dimPx;pY *= dimPx
+     
+     context.fillStyle = 'green'
+     
+     for(var i = 0; i < rope.length; i++)
+        for(var j = 0; j < rope[i].length;j++)
+            if(rope[i][j])
+                context.fillRect(pX+(j*dimPx) ,pY+(i*dimPx) , dimPx , dimPx)
+                
+     context.restore()
+   }
+   
+   function drawJumpManRight(pX , pY) {
+     context.save()
+     
+     var head = []
+     var body = []
+     var hand = []
+     var leg = []
+     var feet = []
+     
      pX *= dimPx;pY *= dimPx
      
      context.fillStyle = 'green'
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var width  = null
 function initGame() {
      canvas =  document.getElementById('canJumpMan')
      context = canvas.getContext('2d')
-     dimPx = 9
+     dimPx = 4.5
      width = canvas.width
      height = canvas.height
      initAnimation()
@@ -62,7 +62,12 @@ function initRopes() {
 }
 
 function initJumpMan() {
-    
+    var points = [{x:80, y:20}]
+
+	for(var i = 0; i < points.length; i++) {
+	   console.log(points[i].x + "," + points[i].y)
+	   drawJumpMan(points[i].x , points[i].y)         
+	}
 }
 
 function initBombs() {
