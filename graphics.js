4af0655e99e81cef53426fc0f15a38bbf44d972a
Alexandre Arsenault <alexandrearsenault@Alexandres-MacBook-Pro.local>
2016-03-01 11:10:09 -0500

    test

--- graphics.js
+++ graphics.js
@@ -33,11 +33,7 @@
    function drawBomb(pX , pY) {
     console.log("alex")
    }
-<<<<<<< HEAD
    
    function test() {
        
    }
-=======
-
->>>>>>> 6187449c929b092aac709c8ed97e3f61d6a04298
