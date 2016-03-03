6fc80743940718254bff4d5df87d845b2f1999d2
tial1212 <alexandrearsenault@Alexandres-MacBook-Pro.local>
2016-03-03 15:49:11 -0500

    ajout sonds

---
+++ soundCore.js
@@ -0,0 +1,28 @@
+
+/*
+  play all the sounds 
+*/
+  var sounds = new Object()
+
+  function initSounds() {
+      var objSon = document.createElement('audio')
+      objSon.setAttribute('src', 'sounds/JM1.mp3')
+      objSon.load()
+    sounds.jump = objSon
+
+      objSon = document.createElement('audio')
+      objSon.setAttribute('src', 'sounds/JM2.mp3')
+      objSon.load()
+    sounds.bomb = objSon;
+
+      objSon = document.createElement('audio')
+      objSon.setAttribute('src', 'sounds/JM3.mp3')
+      objSon.load()
+    sounds.rope = objSon
+
+      objSon = document.createElement('audio')
+      objSon.setAttribute('src', 'sounds/JM4.mp3')
+      objSon.load()
+    sounds.death = objSon;
+  }
+
